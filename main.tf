1b0f4fb11c5218e6bf1362413e3aeb34bf7c8115
kiyoshi_inoue <106907215+rossi0114@users.noreply.github.com>
2023-03-30 19:06:34 +0900

    2st commi
    change public to private

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "github" {
+    token = "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"
+}
+
+resource "github_repository" "example_repo" {
+  name        = "example-repo"
+  description = "An example GitHub repository created with Terraform"
+  private     = True
+  has_issues  = False
+  has_projects = False
+  has_wiki    = False
+}
