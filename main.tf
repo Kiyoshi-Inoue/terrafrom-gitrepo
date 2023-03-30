bbd0f557a7db7a47f5129e664c5c922b9c0747e2
kiyoshi_inoue <106907215+rossi0114@users.noreply.github.com>
2023-03-30 20:31:32 +0900

    12st commit

--- main.tf
+++ main.tf
@@ -8,14 +8,24 @@ terraform {
 }
 
 provider "github" {
-    token=${{ secrets.TF_GITACT}}
+    token="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"
 }
 
-resource "github_repository" "example_repo" {
-  name="example-repo"
-  description="An example GitHub repository created with Terraform"
-  private=True
-  has_issues=False
-  has_projects=False
-  has_wiki=False
+# resource "github_repository" "example_repo" {
+#   name="example-repo"
+#   description="An example GitHub repository created with Terraform"
+#   private=True
+#   has_issues=False
+#   has_projects=False
+#   has_wiki=False
+# }
+
+
+module "repository" {
+  source  = "mineiros-io/repository/github"
+  version = "~> 0.18.0"
+
+  name               = "example-repository"
+  license_template   = "apache-2.0"
+  gitignore_template = "Terraform"
 }
