1067bac95c09f7d22473822bce74a66e469909bd
kiyoshi_inoue <106907215+rossi0114@users.noreply.github.com>
2023-03-30 20:52:58 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,3 @@
-# terraform {
-#   required_providers {
-#     github = {
-#       source  = "integrations/github"
-#       version = "~> 5.0"
-#     }
-#   }
-# }
-
-# provider "github" {
-#     token="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"
-# }
-
-# resource "github_repository" "example_repo" {
-#   name="example-repo"
-#   description="An example GitHub repository created with Terraform"
-#   private=True
-#   has_issues=False
-#   has_projects=False
-#   has_wiki=False
-# }
-
-
 module "repository" {
   source  = "mineiros-io/repository/github"
   version = "~> 0.18.0"
